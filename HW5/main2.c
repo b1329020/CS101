d5b65845af9b06ddd5a801eb5297ba3f7a51d590
b1329020 <kevinq22345@gmail.com>
2024-12-11 16:51:27 +0800

    Update main2.c

--- HW5/main2.c
+++ HW5/main2.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+    double pi = 0.0;   
+    int x = 0;         
+    double term;       
+    double target = 3.14159; 
+    do {
+        term = pow(-1, x) * (4.0 / (2 * x + 1)); 
+        pi += term;
+        x++;          
+    } while (fabs(pi - target) >= 0.00001); 
+    printf(" π = %.5f\n", pi);
+    printf("最小的 x 值 = %d\n", x);
+    return 0;
+}
