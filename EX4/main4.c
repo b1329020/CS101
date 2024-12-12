e99d3dfe5857addc52584428ecf77399d05f6266
b1329020 <kevinq22345@gmail.com>
2024-12-12 15:27:37 +0800

    Update main4.c

--- EX4/main4.c
+++ EX4/main4.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <math.h> 
 
+int main() {
+    int a = 12345;
+    int x,y;
+    x = a%10;
+    if(a<1000){
+        y = 0;
+        a = a-x;
+        a = a+x*1000;
+    }
+    else{
+        y = (a/1000)%10;
+        a = a-x+y;
+        a = a-(y*1000)+(x*1000);
+    }
+    
+    printf("%d",a);
+
+    return 0;
+}
