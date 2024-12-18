2ccafbee3a67382a44c843a13c456c47410f4e77
b1329020 <kevinq22345@gmail.com>
2024-12-19 00:36:21 +0800

    Update main6.c

--- EX3/main6.c
+++ EX3/main6.c
@@ -1,7 +1,7 @@
-#include <studio.h>
+#include <stdio.h>
 
 int main(){
-    int i = 1599;
+    int i = 3000;
     if (i <= 1500){
         printf("70元");
     }
@@ -9,8 +9,11 @@ int main(){
         int n = i - 1500;
         if (n%100){
             int h = ((n/100)+1) * 10;
+            printf("%d 元",70+h);
+        }
+            else {
             printf("%d 元",70+(n/100) * 10);
         }
     }
     return 0;
-}    
+}        
