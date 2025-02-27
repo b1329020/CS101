1e6ba635742e6c46cef8d6e208cb31a278884f9f
b1329020 <kevinq22345@gmail.com>
2025-02-27 23:26:13 +0800

    Update main2.c

--- hw1/main2.c
+++ hw1/main2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+
+void run_length_decoding(const char *encoded) {
+    while (*encoded) {
+        char ch = *encoded++;  
+        int count = *encoded - '0';  
+        encoded++;  
+
+        for (int i = 0; i < count; i++) {
+            putchar(ch); 
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    char input[] = "A4B1C3f3"; 
+    run_length_decoding(input);
+    return 0;
+}
 
