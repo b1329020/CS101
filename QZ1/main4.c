654da3bbe5d2480ff5ff3ff8594774391b0313c3
b1329020 <kevinq22345@gmail.com>
2025-02-27 14:02:28 +0800

    Update main4.c

--- QZ1/main4.c
+++ QZ1/main4.c
@@ -1,1 +1,12 @@
 
+#include <stdio.h>
+
+int main()
+{
+    for(int i = 1,j = 0;i<=9;){
+        printf("%d*%d\t",i,j,i*++j):i;
+        (j==9) ? (i++,j=0,printf("\n")) : i;
+    }
+
+    return 0;
+}
