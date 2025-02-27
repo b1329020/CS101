52099afc8fa2c00c61bf9ee3d1c6ce857fde3b22
b1329020 <kevinq22345@gmail.com>
2025-02-27 14:00:29 +0800

    Update main1.c

--- QZ1/main1.c
+++ QZ1/main1.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main()
+{
+    int i = 3101;
+    int a = i -1500;
+    if(a <= 0){
+        print("70元\n")
+    }
+    else if(a<=100){
+        print("80元\n")
+    }
+    else{
+        int c = a/100;
+        int r = (a%100)?1:0;
+        int sum = (c+r)*10 + 70;
+        printf("%d元\n",sum);
+    }
+
+    return 0;
+}
