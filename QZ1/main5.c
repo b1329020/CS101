28537d40c1a3368abb11f2511fb335644e16fcc2
b1329020 <kevinq22345@gmail.com>
2025-02-27 14:02:44 +0800

    Update main5.c

--- QZ1/main5.c
+++ QZ1/main5.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main()
+{
+    int n = 7;
+    int c = 0;
+    while (n){
+        if(n&1){
+            c++;
+        }
+        n>>1;
+    }
+    printf("%d\n",c);
+
+    return 0;
+}
