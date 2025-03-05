f6a38735ff3312ba97b18eae7f08110f28c6fa6a
b1329020 <kevinq22345@gmail.com>
2025-03-05 22:08:54 +0800

    Update main1.c

--- hw1/main1.c
+++ hw1/main1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+void run_length_encoding(const char *input) {
+    while (*input) {
+        char ch = *input;
+        int count = 0;
+        while (*input == ch) { 
+            count++;
+            input++;
+        }
+        printf("%c%d", ch, count);
+    }
+    printf("\n"); 
+}
+int main() {
+    char input[] = "AABBBCCCCddd"; 
+    run_length_encoding(input);   
+  return 0;
+}
 
