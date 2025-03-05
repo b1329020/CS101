42471eee645cde54b6901340d55327a71676a858
b1329020 <kevinq22345@gmail.com>
2025-03-05 22:43:15 +0800

    Update main3.c

--- hw1/main3.c
+++ hw1/main3.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[] = "10001111";  
+    int ans = 0;
+    for (int i = 0; i < 8; i++) {
+        if (a[i] == '1') {
+            ans += 1 << (7 - i);  // 用位元運算計算 2^(7-i)
+        }
+    }
+    printf("%X\n", ans);  
+    return 0;
+}
 
